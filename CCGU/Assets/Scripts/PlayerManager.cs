2e16cbdb005b905278b4165b065e1b332fc80c1c
Inmortis <122691702+Inmortis@users.noreply.github.com>
2024-05-29 15:00:03 +0500

    0.62
    Добавлены карты

--- CCGU/Assets/Scripts/PlayerManager.cs
+++ CCGU/Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,14 @@ public class PlayerManager : NetworkBehaviour
 {
     public GameObject Card1;
     public GameObject Card2;
+    public GameObject Card3;
+    public GameObject Card4;
+    public GameObject Card5;
+    public GameObject Card6;
+    public GameObject Card7;
+    public GameObject Card8;
+    public GameObject Card9;
+    public GameObject Card10;
     public GameObject PlayerArea;
     public GameObject EnemyArea;
     public GameObject DropZone;
@@ -26,6 +34,15 @@ public class PlayerManager : NetworkBehaviour
     {
         cards.Add(Card1);
         cards.Add(Card2);
+        cards.Add(Card3);
+        cards.Add(Card4);
+        cards.Add(Card5);
+        cards.Add(Card6);
+        cards.Add(Card7);
+        cards.Add(Card8);
+        cards.Add(Card9);
+        cards.Add(Card10);
+
     }
     
     [Command]
